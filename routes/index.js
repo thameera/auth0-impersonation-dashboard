3355c352e0919f850b764f6c7ff3175fd95a39a4
Thameera Senanayaka <thameera123@gmail.com>
2016-11-20 21:57:11 +0530

    Fix callback URL

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,22 @@ var router = express.Router();
 
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || '/callback'
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
+};
+
+var getCallbackURL = function(req) {
+	return req.protocol + '://' + req.get('host') + '/callback';
 };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	env.AUTH0_CALLBACK_URL = getCallbackURL(req);
   res.render('index', { title: 'Auth0 Impersonation Dashboard', env: env });
 });
 
 router.get('/login',
   function(req, res){
+		env.AUTH0_CALLBACK_URL = getCallbackURL(req);
     res.render('login', { env: env });
   });
 
